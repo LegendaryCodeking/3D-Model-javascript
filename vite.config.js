41fca7c259266535d448ca9b8ec486a33afc671c
LegendaryCodeking <legendarycodeking0908@gmail.com>
2023-09-13 16:41:05 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    rollupOptions: {
+        output: {
+            manualChunks: {
+                'vendor': [
+                    'react',
+                ]
+            }
+        }
+    }
+}
 })
